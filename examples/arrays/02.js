3236f2b39bb0314fa1a81507af5a03d23e098777
Андрей Беляков <ttxiom@gmail.com>
2025-05-29 09:30:35 +0500

    up

--- examples/arrays/02.js
+++ examples/arrays/02.js
@@ -1,3 +1,28 @@
 console.log( Boolean([]) ) // true
 console.log(![]) // false
 console.log([] == ![])  // true
+
+// = = = = = = = 
+
+const arr = [1, 2, 3, 77, 9];
+
+console.log(arr);
+console.log(arr.valueOf());
+
+console.log(arr.toString());
+console.log(arr + 5);
+
+// = = = = = = = 
+
+// таблица эмодзи
+// https://snipp.ru/handbk/emoji
+
+let str = "😶🐱👽";
+
+console.log(
+    str.split('').reverse().join('')
+);  // �🐱😶� - тут ломается
+
+console.log(
+    Array.from(str).reverse().join('')
+);  // 👽🐱😶
