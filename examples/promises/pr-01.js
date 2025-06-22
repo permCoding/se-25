cecd0b528ef67d196a8d1f6650359ef33dfdbe69
permCoding <ttxiom@gmail.com>
2025-06-22 10:10:31 +0500

    pr

---
+++ examples/promises/pr-01.js
@@ -0,0 +1,25 @@
+const log = console.log;
+
+const callback = (resolve, reject) => {
+    setTimeout(() => {
+        try {
+            let num = Math.random();
+            if (num < .5) {
+                log('>> resolve');
+                resolve(`num = ${num.toFixed(3)}`);
+            } else {
+                log('>> reject');
+                throw new Error('error number');
+            }
+        } catch (err) {
+            reject(err);
+        }
+    });
+}
+
+const ex_01 = () => new Promise(callback);
+
+ex_01()
+    .then(value => log(value))
+    .catch(error => log(error))
+    .finally(() => log('>> end of the test'));
