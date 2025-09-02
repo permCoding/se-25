ec80e3bcc2c573e22745aaa9bfa4ca5a34f9c313
permCoding <ttxiom@gmail.com>
2025-09-02 12:48:13 +0500

    IIFE

--- part_1/lections/01-Node.js/ex_00.js
+++ part_1/lections/01-Node.js/ex_00.js
@@ -1,5 +1,19 @@
+function funcA(value) {
+    console.log(value**2)
+}
+
+const funcB = () => {};
+
 let n = 10;
 
 (function(value) {
     console.log(value**2)
-})(n);
+})(n); // замыкание для переменной n
+
+(value => console.log(value**3))(n);
+
+/*
+До появления let и const (ES6) IIFE - Immediately Invoked Function Expression
+был основным способом создания блочной области видимости
+с изоляцией переменных и функций
+*/
