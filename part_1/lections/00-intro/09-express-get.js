8c3afc1b7627b39d67e297f39e189c94653dbe6d
permCoding <ttxiom@gmail.com>
2025-09-03 11:27:48 +0500

    up

--- part_1/lections/00-intro/09-express-get.js
+++ part_1/lections/00-intro/09-express-get.js
@@ -3,8 +3,10 @@ const express = require('express'),
     HOST = 'localhost'
     PORT = 3000,
     callbackGet = (req, resp) => { resp.send(' = callback = \nGet') },
+    callbackAbc = (req, resp) => { resp.send(' = callback = \nABC') },
     callbackListen = () => { console.log(`http://${HOST}:${PORT}/`) }
 
 app.get('/', callbackGet);
+app.get('/abc', callbackAbc);
 
 app.listen(PORT, HOST, callbackListen);
---
+++ part_1/lections/01-Node.js/test.js
@@ -0,0 +1,6 @@
+const getValue = (a, b) => {
+    return a++ + --b;
+}
+
+console.log(getValue(5, 10));
+
