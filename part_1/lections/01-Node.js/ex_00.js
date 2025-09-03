1912ffe470ee46e79b1789ba60c10e2a898c4be8
permCoding <ttxiom@gmail.com>
2025-09-03 08:57:56 +0500

    up

--- part_1/lections/01-Node.js/ex_00.js
+++ part_1/lections/01-Node.js/ex_00.js
@@ -5,7 +5,9 @@ const x3 = +Math.random().toFixed(2);
 console.log(x1, x2, x3, typeof x3);
 
 function scopeOut() { // выход за пределы блочной области видимости
-    var arr = [1,2,3,4,5];
+    // var arr = [1,2,3,4,5];
+    // var arr = new Array(1,2,3,4,5);
+    var arr = new Array(5).fill(0).map((value, index) => ++index);
     for (var i = 0; i < arr.length; i++) {
         var value = arr[i];
         console.log(value);
@@ -13,4 +15,21 @@ function scopeOut() { // выход за пределы блочной обла�
     console.log('outside loop', i, value);
     console.log(`outside loop, i=${i}, value=${value}`);
 }
+
 scopeOut();
+{
+    var a = 12;
+    var a = '12';
+    var a = true;
+    var a = { id: 123, name: 'John' };
+    // let a = []; // error - нельзя переопределить
+}
+// let a = 666; // error - нельзя переопределить
+
+// let b = 0;
+{
+    let b = 707;
+    console.log(b);
+}
+let b = 909;
+console.log(b);
