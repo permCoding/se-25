ec8107d5f1c91022530b1636cfece00053a60fec
permCoding <ttxiom@gmail.com>
2025-09-05 13:32:14 +0500

    up

--- part_1/lections/00-intro/09-express-get.js
+++ part_1/lections/00-intro/09-express-get.js
@@ -1,12 +1,16 @@
 const express = require('express'),
     app = express(),
     HOST = 'localhost'
-    PORT = 3000,
-    callbackGet = (req, resp) => { resp.send(' = callback = \nGet') },
-    callbackAbc = (req, resp) => { resp.send(' = callback = \nABC') },
-    callbackListen = () => { console.log(`http://${HOST}:${PORT}/`) }
+    PORT = 3000;
+
+const callbackGet = (req, resp) => { resp.send(' = callback = \nGet') };
+
+const callbackListen = () => { console.log(`http://${HOST}:${PORT}/`) };
 
 app.get('/', callbackGet);
-app.get('/abc', callbackAbc);
 
 app.listen(PORT, HOST, callbackListen);
+
+/*
+http://localhost:3000/
+*/
---
+++ part_1/lections/01-Node.js/pinb_01.js.js
@@ -0,0 +1,21 @@
+const func = (a, b) => {
+    //
+    //
+    return a**b;
+}
+
+let res = func(2, 10);
+
+console.log(`res = ${res}`);
+
+/*
+https://github.com/permCoding/se-25
+
+https://nodejs.org/
+
+https://yandex.ru/pogoda/ru/50?lat=-08.010454 & lon=56.229441
+
+
+https://market.yandex.ru/search?text=смартфон
+
+*/
