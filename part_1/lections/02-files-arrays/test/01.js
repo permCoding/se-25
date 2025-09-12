e5278fd1a00ff5c4626bec60a1400c28bf5750c4
permCoding <ttxiom@gmail.com>
2025-09-12 13:22:47 +0500

    lect

---
+++ part_1/lections/02-files-arrays/test/01.js
@@ -0,0 +1,21 @@
+const log = console.log;
+
+const funcA = (num) => num%2 == 0? num: num+1;
+const power = (a, b) => a**b;
+
+log(funcA(125));
+log(power(2, 10));
+
+// let num = 13;
+// let arr = [];
+
+// new Array(10).fill(0).forEach((e, i) => {})
+
+// console.log(num%2 == 0? funcA(num): funcB(num));
+
+// arr.push(num%2 == 0? true: false);
+
+// arr[100] = arr[0];
+
+// console.log(arr, arr.length);
+
---
+++ part_1/lections/02-files-arrays/test/01.py
@@ -0,0 +1,9 @@
+import array
+
+
+array.array('i', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+
+arr = [];
+
+arr[100] => arr[0]+100*2
