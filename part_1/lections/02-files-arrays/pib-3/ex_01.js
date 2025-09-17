bddeec5f60ac22e8062407667a76dcb0b693580d
permCoding <ttxiom@gmail.com>
2025-09-17 10:52:32 +0500

    lect

---
+++ part_1/lections/02-files-arrays/pib-3/ex_01.js
@@ -0,0 +1,52 @@
+function binToDec_(bin) {
+    if (bin === '') return 0;
+    return binToDec(bin.slice(0, -1)) * 2 + +bin.slice(-1);
+}
+
+function binToDec(bin, p = 0) {
+    if (bin === '') return 0;
+    return binToDec(bin.slice(0, -1), p+1) +  +bin.slice(-1) * 2**p;
+}
+
+const ToDec = (bin, base=2, p=0) => 
+    bin? ToDec(bin.slice(0, -1), base, p+1) + +(bin.slice(-1)) * base**p: 0;
+
+
+console.log(binToDec_('1101'));
+console.log(binToDec('1101'));
+console.log(ToDec('1101'));
+console.log(ToDec('1101', 2));
+console.log(ToDec('10', 8));
+console.log(ToDec('12', 8));
+console.log((13).toString(2));
+
+
+
+// '1101' => 13(10)
+// 1101 2**0
+// 110  2**1
+// 11   2**2
+// 1    2**3
+// ''
+
+/*
+
+'1100'(2) => 12(10)
+'0101'(2) => 5(10)
+
+
+binToDec()
+octToDec()
+hexToDec()
+toDec()
+getDec()
+
+
+
+class BinToDec
+
+*/
+
+
+// const result = ('') ? '+++': '---';
+// console.log(result);
