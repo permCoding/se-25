1f463866c8fa8522ea0e297ad796c8e2ad325c92
permCoding <ttxiom@gmail.com>
2026-03-02 20:51:27 +0500

    p

---
+++ part_2/lections/01-regular-expressions/06-count.js
@@ -0,0 +1,34 @@
+const log = console.log;
+
+const ex_01 = () => { // подсчитать кол-во согласных
+    const str = 'ABcDeFO!';
+    // const str = 'AOIAO';
+
+    const regex = /[BCDF-HJ-NP-TV-Z]/g;
+    const matches = str.match(regex);
+    // log(matches); // 'AOIAO' => null
+    const count = matches ? matches.length : 0;
+    log(count); // 3
+}
+
+const ex_02 = () => { // подсчитать кол-во согласных
+    const str = 'АбраКАДабрА!';
+
+    const regex = /[БВГДЖЗКЛМНПРСТФЗХЦЧШЩ]/g;
+    const matches = str.match(regex);
+    const count = matches ? matches.length : 0;
+    log(count); // 3
+}
+
+const ex_03 = () => { // подсчитать кол-во чисел, которые могут быть двоичными
+    const line = "10 AF 808 101 11 10000001 23F 1!";
+    // const line = '234 FFF A0 70';
+
+    const reB = /\b[01]+\b/g;
+    log([...line.matchAll(reB)].length);
+    log([...line.matchAll(reB).map(m => m[0])]);
+}
+
+ex_01();
+ex_02();
+ex_03();
