58aa04d01e1e65ca2553da18c4dd0fe6c69f8424
permCoding <ttxiom@gmail.com>
2026-05-14 09:00:32 +0500

    p

--- part_2/lections/pib31-express/routers/rUsers.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const ctrlUsers = require('../controllers/ctrlUsers');
-
-router.post('/order', ctrlUsers.orderUsers);
-router.get('/', ctrlUsers.mainUsers);
-router.get('/saveUsers', ctrlUsers.saveUsers);
-
-module.exports = router;
